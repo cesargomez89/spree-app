91f86b8ecd0e8b58333607acfd75f68b346a6aac
Cesar Gomez <cesargomez89@gmail.com>
2013-06-26 17:37:03 -0500

    first commit - empty rails app

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '3.2.13'
+
+gem 'sqlite3'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails'   
+end
+
+gem 'jquery-rails'
+
+gem 'spree'
+gem 'spree_blue_theme', :git => 'git://github.com/spree/spree_blue_theme.git'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano', group: :development
+
+# To use debugger
+# gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreeApp::Application.config.secret_key_base = '1fdf73b0e9c852f3ae3d1be3107cccdb2c60a06da7aa4eaa7d0bb142bdff14c30d53d8ca971dbd09ab89c2c2987d6667e6d561225393a11ff8058d246a4f30ca'
